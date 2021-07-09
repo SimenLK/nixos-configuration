253f98b866dafb2b5668289f94d16a6887c368d6
System administrator <root@zelda.itpartner.no>
2021-07-09 15:32:10 +0200

    Jonas made changes

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,123 @@
 { pkgs, config, ...}:
+let
+    yubikey = {
+      slot = 2;
+      twoFactor = false;
+      storage = {
+        device = "/dev/nvme0n1p2";
+      };
+    };
+in
 {
   networking = {
-    hostName = "nixos";
-    domain = "local";
-    search = [ "local" ];
+    hostName = "zelda";
+    domain = "itpartner.no";
+    search = [ "itpartner.intern" "itpartner.no" ];
+    firewall.allowedTCPPorts = [];
+    firewall.extraCommands = ''
+      iptables -I INPUT -s 10.1.2.40 -j DROP
+    '';
   };
 
   security.pam.services.sshd.googleAuthenticator.enable = true;
 
+  # boot.initrd.luks.yubikeySupport = true;
+  boot.initrd.luks.devices = {
+    luksroot = {
+      device = "/dev/nvme0n1p1";
+      preLVM = true;
+      allowDiscards = true;
+      # inherit yubikey;
+    };
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+
   features = {
-    desktop.enable = false;
-    desktop.keybase.enable = false;
+    desktop.enable = true;
+    desktop.keybase.enable = true;
     cachix.enable = true;
-    nodeExporter.enable = false;
+
+    pki = {
+      enable = false;
+      certmgr.enable = true;
+      certs = {
+        foo = { hosts = [ "localhost" ]; };
+      };
+    };
 
     os = {
       boot = {
         uefi = true;
-        device = "/dev/sda";
+        device = "/dev/nvme0n1";
       };
 
       networkmanager.enable = true;
       externalInterface = "eno2";
 
       docker.enable = false;
+      containerd.enable = true;
 
       adminAuthorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKiAS30ZO+wgfAqDE9Y7VhRunn2QszPHA5voUwo+fGOf jonas-3"
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDULdlLC8ZLu9qBZUYsjhpr6kv5RH4yPkekXQdD7prkqapyoptUkO1nOTDwy7ZsKDxmp9Zc6OtdhgoJbowhGW3VIZPmooWO8twcaYDpkxEBLUehY/n8SlAwBtiHJ4mTLLcynJMVrjmTQLF3FeWVof0Aqy6UtZceFpLp1eNkiHTCM3anwtb9+gfr91dX1YsAOqxqv7ooRDu5rCRUvOi4OvRowepyuBcCjeWpTkJHkC9WGxuESvDV3CySWkGC2fF2LHkAu6SFsFE39UA5ZHo0b1TK+AFqRFiBAb7ULmtuno1yxhpBxbozf8+Yyc7yLfMNCyBpL1ci7WnjKkghQv7yM1xN2XMJLpF56v0slSKMoAs7ThoIlmkRm/6o3NCChgu0pkpNg/YP6A3HfYiEDgChvA6rAHX6+to50L9xF3ajqk4BUzWd/sCk7Q5Op2lzj31L53Ryg8vMP8hjDjYcgEcCCsGOcjUVgcsmfC9LupwRIEz3aF14AWg66+3zAxVho8ozjes= jonas.juselius@juselius.io"
       ];
     };
 
-    lan.enable = false;
+    lan = {
+      enable = true;
+
+      samba.extraConfig = ''
+        netbios name = ${config.networking.hostName}
+        workgroup = ITPARTNER
+        # add machine script = /run/current-system/sw/bin/useradd -d /var/empty -g 65534 -s /run/current-system/sw/bin/false -M %u
+      '';
+
+      krb5 = {
+        enable = true;
+        default_realm = "ITPARTNER.INTERN";
+
+        domain_realm = {
+          "itpartner.no" = "ITPARTNER.INTERN";
+          ".itpartner.no" = "ITPARTNER.INTERN";
+        };
+
+        realms = {
+          "ITPARTNER.INTERN" = {
+            admin_server = "itp-dc3.itpartner.intern";
+            kdc = "itp-dc3.itpartner.intern";
+          };
+        };
+      };
+    };
+  };
+
+  services.dnsmasq.enable = false;
+  services.dnsmasq.extraConfig = ''
+    address=/.cluster.local/10.101.0.1
+  '';
+
+  programs.singularity.enable = true;
+
+  # nix = {
+  #    package = pkgs.nixFlakes;
+  #    extraOptions = pkgs.lib.optionalString (config.nix.package == pkgs.nixFlakes)
+  #      "experimental-features = nix-command flakes";
+  # };
+
+  hardware.bluetooth.config = {
+    General = {
+      AutoConnct = true;
+      MultiProfile = "multiple";
+    };
+  };
+
+  services.pcscd.enable = false; # For Yubikey ykman
+
+  security.pam.yubico = {
+    enable = false;
+    mode = "client"; # "challenge-response";
+    id = "";
+    control = "sufficient";
   };
 
   imports = [
--- modules/desktop.nix
+++ modules/desktop.nix
@@ -34,7 +34,7 @@ let
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.displayManager.job.logToFile = true;
     services.xserver.desktopManager.xterm.enable = true;
-    services.xserver.wacom.enable = true;
+    services.xserver.wacom.enable = false;
 
     services.upower.enable = true;
 
