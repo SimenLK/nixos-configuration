1463438f978bdefc7b090d5d42db8bf513264836
Simen Kirkvik <simen@kirkvik.no>
2022-01-05 16:23:36 +0100

    fix nixos error

--- users.nix
+++ users.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
 {
+  users.groups.admin = {};
+
   users.extraGroups = {
     simkir = { gid = 1000; };
   };
 
   users.users.admin = {
     isSystemUser = true;
+    group = "admin";
   };
 
   users.users.simkir = {
