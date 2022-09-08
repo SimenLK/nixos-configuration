5a91386bb9f156d62c448feb662cdc5ec8299011
Simen Kirkvik <simen@kirkvik.no>
2022-09-08 09:19:04 +0200

    move nuta-nixer package to packages
    try to start systemd service

---
+++ modules/services.nix
@@ -0,0 +1,64 @@
+{ pkgs, config, lib, ... }:
+with lib;
+let
+  cfg = config.services.nuta-nixer;
+  nuta-nixer = pkgs.nuta-nixer;
+
+  configuration = {
+
+    # NOTE(SimenLK): create a container for nuta-nixer
+    # containers.nuta-nixer = {
+    #   autoStart = true;
+    #   privateNetwork = true;
+    #   hostAddress = "10.1.2.94";
+    #   localAddress = "10.1.2.95";
+    #   config = { config, ... }: {
+    #     services.nuta-nixer = {
+    #       enable = cfg.nuta-nixer.enable;
+    #       package = pkgs.nuta-nixer;
+    #       hostName = "localhost";
+    #     };
+
+    #     system.stateVersion = "22.05";
+
+    #     system.firewall = {
+    #       enable = true;
+    #       allowedTCPPorts = [ 80 ];
+    #     };
+    #   };
+    # };
+  };
+in
+{
+  options = {
+    services.nuta-nixer = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to run nuta-nixer
+        '';
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    users.users.nuta-nixer = {
+      isSystemUser = true;
+      group = "nuta-nixer";
+      description = "Nuta-nixer daemon user";
+    }
+    users.group.nuta-nixer = {};
+
+    systemd.services.nuta-nixer = {
+      description = "Nuta-nixer Daemon";
+      after = [ "network.target" "systemd-resolved.service" ];
+      wantedBy = [ "multi-user.target" ];
+      path = [ dnsmasq ];
+      serviceConfig = {
+        ExecStart = "${nuta-nixer}/bin/Server";
+        Restart = "on-failure";
+      };
+    };
+  };
+}
